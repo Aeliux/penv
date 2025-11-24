619f6f46dbd38ddfcff6b920806c1295389e5716
Alireza Poodineh <itsaeliux@gmail.com>
2025-11-24 06:05:53 +0000

    Fix shell detection deadlock bug
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- lib/env.sh
+++ lib/env.sh
@@ -127,8 +127,7 @@ env::shell(){
   elif [[ -f "$env_root/bin/sh" ]]; then
     default_cmd=(/bin/sh --login)
   else
-    err "No suitable shell or startup script found in environment"
-    return 1
+    default_cmd=""
   fi
 
   export PENV_ENV_MODE="environment"
@@ -137,6 +136,11 @@ env::shell(){
   # Build final command
   local -a cmd
   if (( $# == 0 )); then
+    if [[ -z "${default_cmd}" ]]; then
+      err "No default shell found in environment: $env_name"
+      info "Specify a command to run, e.g.: ${C_BOLD}penv shell $env_name /bin/sh${C_RESET}"
+      return 1
+    fi
     cmd=("${default_cmd[@]}")
   else
     cmd=( "$@" )
