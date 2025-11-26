58c44e68ba7705a8aa6349f2be8697dfea066523
Alireza Poodineh <itsaeliux@gmail.com>
2025-11-26 23:08:34 +0330

    Fix commands test bugs
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- build/universal/overlay/penv/test.d/99-commands.sh
+++ build/universal/overlay/penv/test.d/99-commands.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-# Test:
+# Test: Essential and Optional Commands
+# Validates presence of critical commands in the rootfs
 
 # Read list of essential commands from /penv/test.d/required.d/
 commands=""
 if [ -d /penv/test.d/required.d/ ]; then
   for f in /penv/test.d/required.d/*; do
-    [ -f "$f" ] && commands="$commands$(cat "$f")"$'\n'
+    [ -f "$f" ] && commands="$commands$(cat "$f")"'\n'
   done
 fi
+
+commands="$commands"':\n'
 
 # Read list of optional commands from /penv/test.d/optional.d/
 if [ -d /penv/test.d/optional.d/ ]; then
   for f in /penv/test.d/optional.d/*; do
-    [ -f "$f" ] && commands=':'$'\n'"$commands$(cat "$f")"$'\n'
+    [ -f "$f" ] && commands="$commands$(cat "$f")"'\n'
   done
 fi
 
 optional=0
+
+
+# Write commands to a temporary file
+tmpfile=$(mktemp)
+printf "%b" "$commands" > "$tmpfile"
 
 while IFS= read -r line || [ -n "$line" ]; do
   # strip comments after fields and trim whitespace
@@ -26,18 +34,18 @@ while IFS= read -r line || [ -n "$line" ]; do
   [ -z "$line" ] && continue       # skip blank
   case "$line" in
     \#*) continue ;;               # skip comment lines
-    \:*) optional=1; line=${line#\:} ;; # mark as optional and remove leading colon
+    \:) optional=1; continue ;;
   esac
   # take first token as command name
   set -- $line
   cmd=$1
-    test_start "Command '$cmd' exists"
-    if test_command_exists "$cmd"; then
-        test_pass
-    else
-        cls=fail
-        [ $optional -eq 1 ] && cls=skip
-        "test_$cls" "Command '$cmd' not found"
-    fi
-    optional=0
-done
+  test_start "Command '$cmd' exists"
+  if test_command_exists "$cmd"; then
+      test_pass
+  else
+      cls=fail
+      [ $optional -eq 1 ] && cls=skip
+      "test_$cls" "Command '$cmd' not found"
+  fi
+done < "$tmpfile"
+rm -f "$tmpfile"
